7e4e5928cec92a416e78ad20a5e8e74e103ab330
Claudio <claudio.rtega2701@gmail.com>
2025-02-17 13:16:45 +0100

    generate a refined 3D model after the preview

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,42 +1,18 @@
-import { use, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { apiClient } from './lib/api-client/client';
-import { useAPI } from './hooks/useAPI';
-import type { Prompt, Resource } from './lib/api-client/types';
 import Scene from "./components/Scene/Scene";
 import "./App.css";
 import { ChatInput } from "./components/ChatInput";
-
-// Example 1: Simple list with loading state
-function PromptsList() {
-  const { data, error, isLoading, execute } = useAPI<Prompt[]>(
-    () => apiClient.listTools()
-  );
-
-  console.log("PromptsList executed: ", data);
-
-  useEffect(() => {
-    execute();
-  }, []);
-
-  if (isLoading) return <div>Loading prompts...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-  
-  return (
-    <div>
-      <h2>Available Prompts</h2>
-      {data && JSON.stringify(data, null, 2)}
-    </div>
-  );
-}
-
 
 function App() {
   const [prompt, setPrompt] = useState<string>("Create a nice tree house");
   const [progress, setProgress] = useState<number | null>(null);
   const [taskId, setTaskId] = useState<string | null>(null);
+  const [previeLoaded, setPreviewLoaded] = useState<boolean>(false);
   const [sceneURL, setSceneURL] = useState<string | undefined>(undefined);
 
   const onChangePrompt = async (prompt: string) => {
+    
     setPrompt(prompt);
 
     const result = await apiClient.generate3D(prompt)
@@ -56,6 +32,9 @@ function App() {
       console.log("API RESULT 2", task_status);
       const result3 = JSON.parse(task_status.content[0].text);
 
+      const progress = result3.progress;
+      setPrompt(`Waiting for model to be generated ${progress}%`);
+
       console.log("API RESULT 3", result3);
       if (result3.finished_at) {
         finished = true;
@@ -63,13 +42,39 @@ function App() {
         console.log("Scene URL: ", result3.model_urls.glb);
         setPrompt("");
       }
-
-      const progress = result3.progress;
-      setPrompt(`Waiting for model to be generated ${progress}%`);
+      
       // sleep 10 sec
       await new Promise(r => setTimeout(r, 10000));
-    
     }
+
+    const refined_result = await apiClient.callTool("create_3d_refine", { preview_task_id: task_id });
+    console.log("Refined result: ", refined_result);
+
+    const refined_task_id  = JSON.parse(refined_result.content[0].text).result;
+    console.log("Refined Task ID: ", refined_task_id);
+
+    let refined_finished = false
+    while (!refined_finished) {
+      const refined_task_status = await apiClient.callTool("get_3d_task", { task_id: refined_task_id });
+
+      console.log("refined_task_status", refined_task_status);
+      const refined_task_result = JSON.parse(refined_task_status.content[0].text);
+
+      const progress = refined_task_result.progress;
+      setPrompt(`Waiting for model to be colorized ${progress}%`);
+
+      console.log("API RESULT 3", refined_task_result);
+      if (refined_task_result.finished_at) {
+        refined_finished = true;
+        setSceneURL(refined_task_result.model_urls.glb);
+        console.log("Scene URL: ", refined_task_result.model_urls.glb);
+        setPrompt("");
+      }
+      
+      // sleep 10 sec
+      await new Promise(r => setTimeout(r, 10000));
+    }
+    setPrompt("");
   };
 
   return (
